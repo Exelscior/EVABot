1eb4de0151a72e713ff2867ad850aaa98bc11614
Joshua Logan <joshua.logan@eveco.re>
2018-04-20 12:23:11 +0200

    Added call to reconnect in case of adb subprocess error.

--- EVABot.py
+++ EVABot.py
@@ -7,19 +7,52 @@ from utils import Vector2, TouchVector2, console
 
 class EVABot(object):
 
+    ORIENTATION_LANDSCAPE = 1
+    ORIENTATION_LANDSCAPE_REVERSED = 3
+    ORIENTATION_PORTRAIT = 0
+    ORIENTATION_LORTRAIT_REVERSED = 2
+
     def __init__(self, screenSize : tuple = (1920, 1080), ip_address : str = None, runFromDevice : bool = False):
         self.runFromDevice = runFromDevice
-        if not ip_address is None:
+        self.ip_address = ip_address
+        if not ip_address is None and not runFromDevice:
             if not isinstance(ip_address, str) or (
                     len(ip_address.split('.')) != 4 or len(ip_address.split('.')[-1].split(':')) != 2):
                 raise ValueError('EVABot : ip_address variable should respect the "x.x.x.x:y" format, "y" being the '
                                  f'port. Received "{ip_address}" instead.')
-            self.runCMD(('adb', 'connect', f'{ip_address}'))
-        self.ip_address = ip_address
+            self.reconnect
         self.screenSize = screenSize
         self._tmpImageFileName = 'screen.dump'
         self._tmpImage = None
         self.viewList = []
+        self.deviceOrientation = self.getDeviceOrientation()
+
+    def getDeviceOrientation(self, input : str = None, setValue : bool = False):
+        if input is None:
+            adb_cmd = ('su', '-c') if self.runFromDevice else ('adb', 'shell')
+            output = subprocess.check_output(adb_cmd + ('dumpsys', 'input'))
+        else:
+            output = input
+        orientationStart = output.find(b'SurfaceOrientation')
+        orientation = int(output[orientationStart + 20]) - ord('0')
+        if setValue:
+            self.deviceOrientation = orientation
+        return orientation
+
+    def getScreenSize(self, orientationCorrection : bool = True, setValue : bool = False):
+        adb_cmd = ('su', '-c') if self.runFromDevice else ('adb', 'shell')
+        output = subprocess.check_output(adb_cmd + ('dumpsys', 'input'))
+        widthStart = output.find(b'SurfaceWidth')
+        heightStart = output.find(b'SurfaceHeight')
+        width = int(output[widthStart + 14:widthStart + 18].strip(b'px'))
+        height = int(output[heightStart + 15:heightStart + 19].strip(b'px'))
+        if orientationCorrection:
+            orientation = self.getDeviceOrientation(input=output, setValue=True)
+            if orientation in (EVABot.ORIENTATION_LANDSCAPE, EVABot.ORIENTATION_LANDSCAPE_REVERSED):
+                width, height = height, width
+        if setValue:
+            self.screenSize = (width, height)
+        return (width, height)
 
     def loadConfFile(self, fileName : str):
         import json
@@ -31,9 +64,18 @@ class EVABot(object):
         self.viewList = tuple(self.viewList)
 
     def getScreen(self):
-        screenData = subprocess.check_output(('adb', 'shell', 'screencap'))
+        if self.runFromDevice:
+            screenData = subprocess.check_output(('su', '-c', 'screencap'))
+        else:
+            screenData = subprocess.check_output(('adb', 'shell', 'screencap'))
         self._tmpImage = Image.frombytes('RGBA', self.screenSize, screenData)
         return True
+
+    def reconnect(self):
+        if self.ip_address is None or self.runFromDevice:
+            return
+        self.runCMD(('adb', 'disconnect'))
+        self.runCMD(('adb', 'connect', f'{self.ip_address}'))
 
     def checkForView(self):
         try:
@@ -43,6 +85,7 @@ class EVABot(object):
             console.print(f'{console.icho.bold}{console.icho.red}'
                           'Error capturing screen'
                           f'{console.icho.normal}', 0)
+            self.reconnect()
             return False
         idx = len(self.viewList)
         for view in self.viewList:
@@ -75,7 +118,7 @@ class EVABot(object):
         elif isinstance(command, (tuple, list)):
             command_array = command
         if self.runFromDevice:
-            subprocess.call(command_array[2:])
+            subprocess.call(('su', '-c', ' '.join(command_array[2:])))
         else:
             subprocess.call(command_array)
 
